306204e2919819b6944b13d2b8133a087a6b4ecf
ptsouth97 <ptsouth97@gmail.com>
2016-12-29 15:46:42 -0500

    added some stuff to app2

--- NumberGuess/app2.py
+++ NumberGuess/app2.py
@@ -1,4 +1,15 @@
+import random
+
 print('--------------------------------')
 print('    GUESS THAT NUMBER APP       ')
 print('--------------------------------')
+print()
 
+the_number = random.randint(0, 100)
+
+guess_text = input('Guess a number between 0 and 100: ')
+
+guess = int(guess_text)
+
+print(guess_text, type(guess_text))
+print(guess, type(guess))
