68aa2b9c8270608e33c85c576d441efbf88a8635
ptsouth97 <ptsouth97@gmail.com>
2016-12-29 20:54:22 -0500

    added birthday countdown app

---
+++ BirthdayCountdown/app3.py
@@ -0,0 +1,23 @@
+def print_header():
+	print('------------------------------')
+	print('  BIRTHDAY COUNTDOWN          ')
+	print('------------------------------')
+
+
+def get_birthday_from_user():
+	pass
+
+
+def compute_days_between_dates():
+	pass
+
+
+def print_birthday_information():
+	pass
+
+
+def main():
+	print_header()
+	get_birthday_from_user()
+	compute_days_between_dates()
+	print_birthday_information()
