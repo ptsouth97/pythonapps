981d447ba240f8eab5262b505ecee311630e3f23
ptsouth97 <ptsouth97@gmail.com>
2016-12-29 20:38:40 -0500

    added while loop to number guess game

--- NumberGuess/app2.py
+++ NumberGuess/app2.py
@@ -6,10 +6,16 @@ print('--------------------------------')
 print()
 
 the_number = random.randint(0, 100)
+guess = -1
 
-guess_text = input('Guess a number between 0 and 100: ')
+name = input('Player what is your name? ')
 
-guess = int(guess_text)
-
-print(guess_text, type(guess_text))
-print(guess, type(guess))
+while guess != the_number: 
+	guess_text = input('Guess a number between 0 and 100: ')
+	guess = int(guess_text)
+	if guess < the_number:
+		print('Sorry {}, your guess of {} was too LOW'.format(name, guess))
+	elif guess > the_number:
+		print('Sorry {}, your guess of {} was too too HIGH'.format(name, guess))
+	else:
+		print('Excellent work {}, you win!'.format(name))
