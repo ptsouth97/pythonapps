dcaf09f4539df007ac92284500199b158dde1d1c
ptsouth97 <ptsouth97@gmail.com>
2017-02-05 16:03:07 -0500

    added readme files

--- HelloWorld/app1.py
+++
@@ -1,8 +0,0 @@
-print('--------------------------------------')
-print('          HELLO APP                   ')
-print('--------------------------------------')
-print()
-
-user_text = input('What is your name? ')
-greeting = 'Nice to meet you {}'.format(user_text)
-print(greeting)
